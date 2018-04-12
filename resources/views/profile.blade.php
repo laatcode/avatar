22df25074fe66a49c97a60fe2fb469e7cfbd1eab
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2018-04-12 15:15:27 -0500

    Formulario para cambio de imagen de pefil en vista de perfil de usuario

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,6 +5,12 @@
       <div class="col">
         <img src="{{ Storage::disk('public')->url($user->avatar) }}" style="width:150px; height: 150px; border-radius:50%; float:left; margin-right:25px;">
         <h2>{{ $user->name }}</h2>
+        <form action="/profile" method="post" enctype="multipart/form-data">
+          {{ csrf_field() }}
+          <label> Actualizar imagen de perfil</label>
+          <input class="d-block" type="file" name="avatar">
+          <input type="submit" class="btn btn-primary btn-sm">
+        </form>
       </div>
     </div>
   </div>
